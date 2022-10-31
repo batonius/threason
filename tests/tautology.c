5d612b72d1f970d884e02e1c3b4c2c85a6c265a8
Egor Karavaev <batonius.kirov@gmail.com>
2022-10-31 22:21:11 +0300

    Add benchmarking

--- tests/tautology.c
+++ tests/tautology.c
@@ -1,4 +1,5 @@
 #include "testing.h"
+#include <stdlib.h>
 
 TEST(zero_equals_zero) {
 	ASSERT_EQ(0, 0);
@@ -8,9 +9,47 @@ TEST(abc_isnt_equal_to_xyz) {
 	ASSERT_STRN_NEQ("abc", "zyz", 3);
 }
 
-TEST_MAIN("Testign framework itself")
+BENCH(malloc_and_free_1Kb)
+	BENCH_MEASURE(1000)
+		char* volatile data = malloc(1024);
+		*data = 0;
+		free(data);
+	END_MEASURE()
+END_BENCH()
+
+BENCH(malloc_and_free_1Kb_loop)
+	BENCH_MEASURE_LOOP(1000)
+		char* volatile data = malloc(1024);
+		*data = 0;
+		free(data);
+	END_MEASURE_LOOP()
+END_BENCH()
+
+BENCH(malloc_and_free_1Gb)
+	BENCH_MEASURE(1000)
+		char* volatile data = malloc(1024 * 1024 * 1024);
+		*data = 0;
+		free(data);
+	END_MEASURE()
+END_BENCH()
+
+BENCH(malloc_and_free_1Gb_loop)
+	BENCH_MEASURE_LOOP(1000)
+		char* volatile data = malloc(1024 * 1024 * 1024);
+		*data = 0;
+		free(data);
+	END_MEASURE_LOOP()
+END_BENCH()
+
+TEST_MAIN()
 	TEST_SUITE("Tautology")
 		zero_equals_zero,
 		abc_isnt_equal_to_xyz,
-	END_SUITE()
+	END_TEST_SUITE()
+	BENCH_SUITE("Allocator")
+		malloc_and_free_1Kb,
+		malloc_and_free_1Gb,
+		malloc_and_free_1Kb_loop,
+		malloc_and_free_1Gb_loop,
+	END_BENCH_SUITE()
 END_MAIN()
--- tests/testing.h
+++ tests/testing.h
@@ -1,12 +1,20 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct {
     const char* test_name;
     size_t total_tests;
     size_t failed_tests;
 } test_result_t;
+
+typedef struct {
+    const char* bench_name;
+    struct timespec elapsed;
+    size_t iterations;
+} bench_result_t;
 
 #define TEST(name)                                                         \
     static void test_function_##name(test_result_t* test_result);          \
@@ -62,10 +70,10 @@ typedef struct {
 
 #define TEST_SUITE(name)                                                     \
     {                                                                        \
-        printf("%s:\n", name);                                               \
+        printf("Testing %s:\n", name);                                       \
         test_result_t suite_results = {.total_tests = 0, .failed_tests = 0}; \
         test_result_t (*__test_functions[])() = {
-#define END_SUITE()                                                       \
+#define END_TEST_SUITE()                                                  \
     }                                                                     \
     ;                                                                     \
     for (size_t i = 0;                                                    \
@@ -84,11 +92,10 @@ typedef struct {
     }                                                                     \
     ;
 
-#define TEST_MAIN(name)               \
+#define TEST_MAIN()                   \
     int main(int argc, char** argv) { \
         (void)argc;                   \
         (void)argv;                   \
-        printf("Testing %s\n", name); \
         test_result_t final_results = {.total_tests = 0, .failed_tests = 0};
 
 #define END_MAIN()                                                      \
@@ -96,3 +103,85 @@ typedef struct {
            final_results.failed_tests);                                 \
     return final_results.failed_tests > 0 ? 1 : 0;                      \
     }
+
+#define BENCH(name)                                                        \
+    static bench_result_t name() {                                         \
+        bench_result_t __result = {.elapsed = {.tv_sec = 0, .tv_nsec = 0}, \
+                                   .iterations = 0,                        \
+                                   .bench_name = #name};
+
+#define BENCH_MEASURE(iters)                              \
+    size_t __iters = (iters);                             \
+    for (volatile size_t __i = 0; __i < __iters; ++__i) { \
+        struct timespec __start_time;                     \
+        clock_gettime(CLOCK_MONOTONIC, &__start_time);
+
+#define END_MEASURE()                                                   \
+    struct timespec __end_time;                                         \
+    clock_gettime(CLOCK_MONOTONIC, &__end_time);                        \
+    __end_time.tv_sec -= __start_time.tv_sec;                           \
+    if (__end_time.tv_nsec < __start_time.tv_nsec) {                    \
+        --__end_time.tv_sec;                                            \
+        __end_time.tv_nsec += 1000000000LL - __start_time.tv_nsec;      \
+    } else {                                                            \
+        __end_time.tv_nsec -= __start_time.tv_nsec;                     \
+    }                                                                   \
+    __result.elapsed.tv_sec += __end_time.tv_sec;                       \
+    __result.elapsed.tv_nsec += __end_time.tv_nsec;                     \
+    }                                                                   \
+    __result.iterations = __iters;                                      \
+    __result.elapsed.tv_sec += __result.elapsed.tv_nsec / 1000000000LL; \
+    __result.elapsed.tv_nsec %= 1000000000LL;
+
+#define BENCH_MEASURE_LOOP(iters)                  \
+    size_t __iters = (iters);                      \
+    struct timespec __start_time;                  \
+    clock_gettime(CLOCK_MONOTONIC, &__start_time); \
+    for (volatile size_t __i = 0; __i < __iters; ++__i) {
+#define END_MEASURE_LOOP()                                              \
+    }                                                                   \
+    struct timespec __end_time;                                         \
+    clock_gettime(CLOCK_MONOTONIC, &__end_time);                        \
+    __end_time.tv_sec -= __start_time.tv_sec;                           \
+    if (__end_time.tv_nsec < __start_time.tv_nsec) {                    \
+        --__end_time.tv_sec;                                            \
+        __end_time.tv_nsec += 1000000000LL - __start_time.tv_nsec;      \
+    } else {                                                            \
+        __end_time.tv_nsec -= __start_time.tv_nsec;                     \
+    }                                                                   \
+    __result.elapsed.tv_sec += __end_time.tv_sec;                       \
+    __result.elapsed.tv_nsec += __end_time.tv_nsec;                     \
+    __result.elapsed.tv_sec += __result.elapsed.tv_nsec / 1000000000LL; \
+    __result.elapsed.tv_nsec %= 1000000000LL;                           \
+    __result.iterations = __iters;
+
+#define END_BENCH()  \
+    return __result; \
+    }
+
+#define BENCH_SUITE(name)                   \
+    {                                       \
+        printf("Benchmarking %s:\n", name); \
+        bench_result_t (*__bench_functions[])() = {
+#define END_BENCH_SUITE()                                                   \
+    }                                                                       \
+    ;                                                                       \
+    for (size_t i = 0;                                                      \
+         i < sizeof(__bench_functions) / sizeof(*__bench_functions); ++i) { \
+        const bench_result_t bench_result = __bench_functions[i]();         \
+        uint64_t elapsed_nsec = bench_result.elapsed.tv_sec * 1000000000 +  \
+                                bench_result.elapsed.tv_nsec;               \
+        uint64_t elapsed_nsec_per_iter =                                    \
+            elapsed_nsec / bench_result.iterations;                         \
+        printf(                                                             \
+            "    %25s: %3lums %3luus %3luns per iter (total %3lums %3luus "        \
+            "%3luns for %zd "                                                \
+            "iters)\n",                                                     \
+            bench_result.bench_name, (elapsed_nsec_per_iter / 1000000),     \
+            (elapsed_nsec_per_iter % 1000000) / 1000,                       \
+            (elapsed_nsec_per_iter % 1000000) % 1000,                       \
+            (elapsed_nsec / 1000000), (elapsed_nsec % 1000000) / 1000,      \
+            (elapsed_nsec % 1000000) % 1000, bench_result.iterations);      \
+    }                                                                       \
+    }                                                                       \
+    ;
