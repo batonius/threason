f089cfe8eeed52e04b55cb642e2772bc7b63732d
Egor Karavaev <batonius.kirov@gmail.com>
2022-11-10 00:57:43 +0300

    Proper inlining

--- include/tags.h
+++ include/tags.h
@@ -32,18 +32,84 @@ typedef uint8_t ThsnTag;
 #define THSN_TAG_TYPE(tag) ((tag) >> 4)
 #define THSN_TAG_SIZE(tag) ((tag)&0x0f)
 
-ThsnResult thsn_vector_store_tagged_value(ThsnVector* /*in/out*/ vector,
-                                          ThsnTag tag, ThsnSlice value_slice);
+inline ThsnResult thsn_vector_store_tagged_value(ThsnVector* /*in/out*/ vector,
+                                                 ThsnTag tag,
+                                                 ThsnSlice value_slice) {
+    BAIL_ON_NULL_INPUT(vector);
+    if (value_slice.size > 0) {
+        BAIL_ON_NULL_INPUT(value_slice.data);
+    }
+    size_t allocation_offset = THSN_VECTOR_OFFSET(*vector);
+    BAIL_ON_ERROR(thsn_vector_grow(vector, sizeof(tag) + value_slice.size));
+    char* allocated_data = THSN_VECTOR_AT_OFFSET(*vector, allocation_offset);
+    memcpy(allocated_data, &tag, sizeof(tag));
+    allocated_data += sizeof(tag);
+    if (value_slice.size > 0) {
+        memcpy(allocated_data, value_slice.data, value_slice.size);
+    }
+    return THSN_RESULT_SUCCESS;
+}
 
-ThsnResult thsn_vector_store_null(ThsnVector* /*in/out*/ vector);
+inline ThsnResult thsn_vector_store_null(ThsnVector* /*in/out*/ vector) {
+    return thsn_vector_store_tagged_value(
+        vector, THSN_TAG_MAKE(THSN_TAG_NULL, 0), THSN_SLICE_MAKE_EMPTY());
+}
 
-ThsnResult thsn_vector_store_bool(ThsnVector* /*in/out*/ vector, bool value);
+inline ThsnResult thsn_vector_store_bool(ThsnVector* /*in/out*/ vector,
+                                         bool value) {
+    return thsn_vector_store_tagged_value(
+        vector,
+        THSN_TAG_MAKE(THSN_TAG_BOOL,
+                      value ? THSN_TAG_SIZE_TRUE : THSN_TAG_SIZE_FALSE),
+        THSN_SLICE_MAKE_EMPTY());
+}
 
-ThsnResult thsn_vector_store_double(ThsnVector* /*in/out*/ vector,
-                                    double value);
+inline ThsnResult thsn_vector_store_double(ThsnVector* /*in/out*/ vector,
+                                           double value) {
+    return thsn_vector_store_tagged_value(
+        vector, THSN_TAG_MAKE(THSN_TAG_DOUBLE, THSN_TAG_SIZE_F64),
+        THSN_SLICE_FROM_VAR(value));
+}
 
-ThsnResult thsn_vector_store_int(ThsnVector* /*in/out*/ vector,
-                                 long long value);
+inline ThsnResult thsn_vector_store_int(ThsnVector* /*in/out*/ vector,
+                                        long long value) {
+    if (value == 0) {
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_INT, THSN_TAG_SIZE_ZERO),
+            THSN_SLICE_MAKE_EMPTY());
+    }
+    if (value <= INT8_MAX && value >= INT8_MIN) {
+        const int8_t int8_value = (int8_t)value;
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_INT, sizeof(int8_t)),
+            THSN_SLICE_FROM_VAR(int8_value));
+    }
+    if (value <= INT16_MAX && value >= INT16_MIN) {
+        const int16_t int16_value = (int16_t)value;
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_INT, sizeof(int16_t)),
+            THSN_SLICE_FROM_VAR(int16_value));
+    }
+    if (value <= INT32_MAX && value >= INT32_MIN) {
+        const int32_t int32_value = (int32_t)value;
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_INT, sizeof(int32_t)),
+            THSN_SLICE_FROM_VAR(int32_value));
+    }
+    return thsn_vector_store_tagged_value(
+        vector, THSN_TAG_MAKE(THSN_TAG_INT, sizeof(long long)),
+        THSN_SLICE_FROM_VAR(value));
+}
 
-ThsnResult thsn_vector_store_string(ThsnVector* /*in/out*/ vector,
-                                    ThsnSlice string_slice);
+inline ThsnResult thsn_vector_store_string(ThsnVector* /*in/out*/ vector,
+                                           ThsnSlice string_slice) {
+    if (string_slice.size <= THSN_TAG_SIZE_MAX) {
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_SMALL_STRING, string_slice.size),
+            string_slice);
+    } else {
+        return thsn_vector_store_tagged_value(
+            vector, THSN_TAG_MAKE(THSN_TAG_REF_STRING, THSN_TAG_SIZE_INBOUND),
+            THSN_SLICE_FROM_VAR(string_slice));
+    }
+}
--- include/vector.h
+++ include/vector.h
@@ -15,33 +15,96 @@ typedef struct {
 
 #define THSN_VECTOR_INIT() \
     (ThsnVector) { .buffer = NULL, .capacity = 0, .offset = 0 }
+
 #define THSN_VECTOR_SPACE_LEFT(vector) ((vector).capacity - (vector).offset)
+
 #define THSN_VECTOR_OFFSET(vector) ((vector).offset)
+
 #define THSN_VECTOR_AT_CURRENT_OFFSET(vector) \
     ((vector).buffer + (vector).offset)
+
 #define THSN_VECTOR_AT_OFFSET(vector, offset) ((vector).buffer + (offset))
+
 #define THSN_VECTOR_EMPTY(vector) ((vector).offset == 0)
+
 #define THSN_VECTOR_AS_SLICE(vector) \
     THSN_SLICE_MAKE((vector).buffer, (vector).offset)
-
-ThsnResult thsn_vector_make(ThsnVector* /*out*/ vector, size_t prealloc_size);
-
-ThsnResult thsn_vector_free(ThsnVector* /*in/out*/ vector);
-
-/* Invalidates all the pointers within the vector */
-ThsnResult thsn_vector_grow(ThsnVector* /*in/out*/ vector, size_t data_size);
-
-ThsnResult thsn_vector_shrink(ThsnVector* /*in/out*/ vector,
-                              size_t shrink_size);
-
-ThsnResult thsn_vector_push(ThsnVector* /*in/out*/ vector,
-                            ThsnSlice /*in*/ slice);
 
 #define THSN_VECTOR_PUSH_VAR(vector, var) \
     thsn_vector_push(&(vector), THSN_SLICE_FROM_VAR(var))
 
-ThsnResult thsn_vector_pop(ThsnVector* /*in/out*/ vector,
-                           ThsnSlice /*out*/ slice);
-
 #define THSN_VECTOR_POP_VAR(vector, var) \
     thsn_vector_pop(&(vector), THSN_SLICE_FROM_VAR(var))
+
+inline ThsnResult thsn_vector_make(ThsnVector* /*out*/ vector,
+                                   size_t prealloc_size) {
+    BAIL_ON_NULL_INPUT(vector);
+    vector->buffer = malloc(prealloc_size);
+    BAIL_ON_ALLOC_FAILURE(vector->buffer);
+    vector->capacity = prealloc_size;
+    vector->offset = 0;
+    return THSN_RESULT_SUCCESS;
+}
+
+inline ThsnResult thsn_vector_free(ThsnVector* /*in/out*/ vector) {
+    BAIL_ON_NULL_INPUT(vector);
+    if (vector->buffer != NULL) {
+        free(vector->buffer);
+        vector->buffer = NULL;
+    }
+    vector->capacity = 0;
+    vector->offset = 0;
+    return THSN_RESULT_SUCCESS;
+}
+
+/* Invalidates all the pointers within the vector */
+inline ThsnResult thsn_vector_grow(ThsnVector* /*in/out*/ vector,
+                                   size_t data_size) {
+    BAIL_ON_NULL_INPUT(vector);
+    const size_t allocated_space_left = THSN_VECTOR_SPACE_LEFT(*vector);
+    if (allocated_space_left < data_size) {
+        const size_t addr_space_left = SIZE_MAX - vector->offset;
+        if (addr_space_left < data_size) {
+            return THSN_RESULT_OUT_OF_MEMORY_ERROR;
+        }
+        const size_t grow_size =
+            vector->capacity < data_size || vector->capacity > addr_space_left
+                ? data_size
+                : vector->capacity;
+        vector->buffer = realloc(vector->buffer, vector->capacity + grow_size);
+        BAIL_ON_ALLOC_FAILURE(vector->buffer);
+        vector->capacity += grow_size;
+    }
+    vector->offset += data_size;
+    return THSN_RESULT_SUCCESS;
+}
+
+inline ThsnResult thsn_vector_shrink(ThsnVector* /*in/out*/ vector,
+                                     size_t shrink_size) {
+    BAIL_ON_NULL_INPUT(vector);
+    if (vector->offset < shrink_size) {
+        return THSN_RESULT_INPUT_ERROR;
+    }
+    vector->offset -= shrink_size;
+    return THSN_RESULT_SUCCESS;
+}
+
+inline ThsnResult thsn_vector_push(ThsnVector* /*in/out*/ vector,
+                                   ThsnSlice /*in*/ slice) {
+    BAIL_ON_NULL_INPUT(vector);
+    BAIL_ON_NULL_INPUT(slice.data);
+    size_t pregrow_offset = vector->offset;
+    BAIL_ON_ERROR(thsn_vector_grow(vector, slice.size));
+    memcpy(THSN_VECTOR_AT_OFFSET(*vector, pregrow_offset), slice.data,
+           slice.size);
+    return THSN_RESULT_SUCCESS;
+}
+
+inline ThsnResult thsn_vector_pop(ThsnVector* /*in/out*/ vector,
+                                  ThsnSlice /*out*/ slice) {
+    BAIL_ON_NULL_INPUT(vector);
+    BAIL_ON_NULL_INPUT(slice.data);
+    BAIL_ON_ERROR(thsn_vector_shrink(vector, slice.size));
+    memcpy(slice.data, THSN_VECTOR_AT_CURRENT_OFFSET(*vector), slice.size);
+    return THSN_RESULT_SUCCESS;
+}
